21be9f7fe072355e120aa3229e378883e7232d4f
max milkert <mmilkert@nrel.gov>
2025-02-03 11:28:11 -0700

    updated naming to be more sensible. will change behavior of neuron similarity. right now it will look at both a layer's pre activation and post activation outputs because 'relu' is a named module

--- pyPrune/models/LeNet.py
+++ pyPrune/models/LeNet.py
@@ -1,25 +1,28 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 class LeNet(nn.Module):
     def __init__(self):
         super(LeNet, self).__init__()
         # 1 input channel for mnist images, 6 output channels for this layer. stride defaults to 1
         self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
+        self.relu1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.relu2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.fc1 = nn.Linear(400, 120)
+        self.relu3 = nn.ReLU()
         self.fc2 = nn.Linear(120, 84)
+        self.relu4 = nn.ReLU()
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool1(self.relu1(self.conv1(x)))
+        x = self.pool2(self.relu2(self.conv2(x)))
         x = x.view(-1, 400) # flatten for dense layers
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = self.relu3(self.fc1(x))
+        x = self.relu4(self.fc2(x))
         x = self.fc3(x)
         return x
--- pyPrune/models/ResNet20.py
+++ pyPrune/models/ResNet20.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from collections import OrderedDict
 # Define a Residual Block for ResNet
 class BasicBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1):
@@ -9,24 +9,28 @@ class BasicBlock(nn.Module):
         
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu1 = nn.ReLU()
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu2 = nn.ReLU()
+        
         
         # Skip connection (identity mapping)
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_channels != out_channels:
+        self.shortcut = nn.Sequential() #empty sequential acts as skip connection when channels doesn't change
+        if stride != 1 or in_channels != out_channels: #this is for when it does change
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels)
+                OrderedDict([
+                ('conv1',nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False)),
+                ('bn1',nn.BatchNorm2d(out_channels))
+                ])
             )
-        
+
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.relu1(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
         out += self.shortcut(x)  # Add the skip connection (identity mapping)
-        out = F.relu(out)
+        out = self.relu2(out)
         return out
-
 
 # Define ResNet-20
 class ResNet20(nn.Module):
@@ -36,28 +40,30 @@ class ResNet20(nn.Module):
         # Initial convolution and batch norm
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(16)
+        self.relu1 = nn.ReLU()
         
         # Create layers with residual blocks
-        self.layer1 = self._make_layer(16, 16, 3, stride=1)  # 3 residual blocks
-        self.layer2 = self._make_layer(16, 32, 3, stride=2)  # 3 residual blocks
-        self.layer3 = self._make_layer(32, 64, 3, stride=2)  # 3 residual blocks
+        # chunks are collections of residual blocks with a constant size
+        self.chunk1 = self._make_layer(16, 16, 3, stride=1)  # 3 residual blocks
+        self.chunk2 = self._make_layer(16, 32, 3, stride=2)  # 3 residual blocks
+        self.chunk3 = self._make_layer(32, 64, 3, stride=2)  # 3 residual blocks
         
         # Fully connected layer
         self.fc = nn.Linear(64, num_classes)
     
     def _make_layer(self, in_channels, out_channels, num_blocks, stride):
-        layers = []
-        for _ in range(num_blocks):
-            layers.append(BasicBlock(in_channels, out_channels, stride))
-            in_channels = out_channels
+        layers = OrderedDict()
+        for i in range(num_blocks):
+            layers['block'+str(i)] = BasicBlock(in_channels, out_channels, stride)
+            in_channels = out_channels # number of channles changes once, then stays constant for all blocks in a chunk
             stride = 1  # Keep stride=1 for subsequent blocks in the same layer
-        return nn.Sequential(*layers)
+        return nn.Sequential(layers)
     
     def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))  # Initial convolution
-        x = self.layer1(x)  # Layer 1
-        x = self.layer2(x)  # Layer 2
-        x = self.layer3(x)  # Layer 3
+        x = self.relu1(self.bn1(self.conv1(x)))  # Initial convolution
+        x = self.chunk1(x)  # Layer 1
+        x = self.chunk2(x)  # Layer 2
+        x = self.chunk3(x)  # Layer 3
         
         # Global average pooling
         x = F.adaptive_avg_pool2d(x, (1, 1))
