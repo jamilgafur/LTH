ccea469369bb37617d3ba219b55a9c5f19c17151
MaximusMilkertus <42820122+MaximusMilkertus@users.noreply.github.com>
2025-02-10 08:36:43 -0700

    updated naming to be more sensible. will change behavior of neuron similarity. right now it will look at both a layer's pre activation and post activation outputs because 'relu' is a named module (#18)
    Co-authored-by: max milkert <mmilkert@nrel.gov>

--- pyPrune/models/Vgg16.py
+++ pyPrune/models/Vgg16.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from collections import OrderedDict
 
 class VGG16_CIFAR10(nn.Module):
     def __init__(self, num_classes=10):
@@ -7,57 +9,61 @@ class VGG16_CIFAR10(nn.Module):
         
         # Convolutional layers
         self.features = nn.Sequential(
+            OrderedDict([
             # Block 1
-            nn.Conv2d(3, 64, kernel_size=3, padding=1),  # 32x32x3 -> 32x32x64
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),  # 32x32x64 -> 32x32x64
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # 32x32x64 -> 16x16x64
+            ('conv_1',nn.Conv2d(3, 64, kernel_size=3, padding=1)),  # 32x32x3 -> 32x32x64
+            ('relu_1',nn.ReLU(inplace=True)),
+            ('conv_2',nn.Conv2d(64, 64, kernel_size=3, padding=1)),  # 32x32x64 -> 32x32x64
+            ('relu_2',nn.ReLU(inplace=True)),
+            ('pool_1',nn.MaxPool2d(kernel_size=2, stride=2)),  # 32x32x64 -> 16x16x64
             
             # Block 2
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),  # 16x16x64 -> 16x16x128
-            nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),  # 16x16x128 -> 16x16x128
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # 16x16x128 -> 8x8x128
+            ('conv_3',nn.Conv2d(64, 128, kernel_size=3, padding=1)),  # 16x16x64 -> 16x16x128
+            ('relu_3',nn.ReLU(inplace=True)),
+            ('conv_4',nn.Conv2d(128, 128, kernel_size=3, padding=1)),  # 16x16x128 -> 16x16x128
+            ('relu_4',nn.ReLU(inplace=True)),
+            ('pool_2',nn.MaxPool2d(kernel_size=2, stride=2)),  # 16x16x128 -> 8x8x128
             
             # Block 3
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),  # 8x8x128 -> 8x8x256
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),  # 8x8x256 -> 8x8x256
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),  # 8x8x256 -> 8x8x256
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # 8x8x256 -> 4x4x256
+            ('conv_5',nn.Conv2d(128, 256, kernel_size=3, padding=1)),  # 8x8x128 -> 8x8x256
+            ('relu_5',nn.ReLU(inplace=True)),
+            ('conv_6',nn.Conv2d(256, 256, kernel_size=3, padding=1)),  # 8x8x256 -> 8x8x256
+            ('relu_6',nn.ReLU(inplace=True)),
+            ('conv_7',nn.Conv2d(256, 256, kernel_size=3, padding=1)),  # 8x8x256 -> 8x8x256
+            ('relu_7',nn.ReLU(inplace=True)),
+            ('pool_3',nn.MaxPool2d(kernel_size=2, stride=2)),  # 8x8x256 -> 4x4x256
             
             # Block 4
-            nn.Conv2d(256, 512, kernel_size=3, padding=1),  # 4x4x256 -> 4x4x512
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),  # 4x4x512 -> 4x4x512
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),  # 4x4x512 -> 4x4x512
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # 4x4x512 -> 2x2x512
+            ('conv_8',nn.Conv2d(256, 512, kernel_size=3, padding=1)),  # 4x4x256 -> 4x4x512
+            ('relu_8',nn.ReLU(inplace=True)),
+            ('conv_9',nn.Conv2d(512, 512, kernel_size=3, padding=1)),  # 4x4x512 -> 4x4x512
+            ('relu_9',nn.ReLU(inplace=True)),
+            ('conv_10',nn.Conv2d(512, 512, kernel_size=3, padding=1)),  # 4x4x512 -> 4x4x512
+            ('relu_10',nn.ReLU(inplace=True)),
+            ('pool_4',nn.MaxPool2d(kernel_size=2, stride=2)),  # 4x4x512 -> 2x2x512
             
             # Block 5
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),  # 2x2x512 -> 2x2x512
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),  # 2x2x512 -> 2x2x512
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),  # 2x2x512 -> 2x2x512
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # 2x2x512 -> 1x1x512
+            ('conv_11',nn.Conv2d(512, 512, kernel_size=3, padding=1)),  # 2x2x512 -> 2x2x512
+            ('relu_11',nn.ReLU(inplace=True)),
+            ('conv_12',nn.Conv2d(512, 512, kernel_size=3, padding=1)),  # 2x2x512 -> 2x2x512
+            ('relu_12',nn.ReLU(inplace=True)),
+            ('conv_13',nn.Conv2d(512, 512, kernel_size=3, padding=1)),  # 2x2x512 -> 2x2x512
+            ('relu_13',nn.ReLU(inplace=True)),
+            ('pool_5',nn.MaxPool2d(kernel_size=2, stride=2))  # 2x2x512 -> 1x1x512
+            ])
         )
         
         # Fully connected layers
         self.classifier = nn.Sequential(
-            nn.Linear(512, 4096),  # Flatten 1x1x512 -> 512
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=0.5),
-            nn.Linear(4096, 4096), 
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=0.5),
-            nn.Linear(4096, num_classes),  # Output for CIFAR-10 classes (10 classes)
+            OrderedDict([
+            ('fc_1',nn.Linear(512, 4096)),  # Flatten 1x1x512 -> 512
+            ('relu_1',nn.ReLU(inplace=True)),
+            ('dropout_1',nn.Dropout(p=0.5)),
+            ('fc_2',nn.Linear(4096, 4096)), 
+            ('relu_2',nn.ReLU(inplace=True)),
+            ('dropout_2',nn.Dropout(p=0.5)),
+            ('fc_3',nn.Linear(4096, num_classes)) # Output for CIFAR-10 classes (10 classes)
+            ])
         )
 
     def forward(self, x):
