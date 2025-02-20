71cd80a14fb831c7c384991032fd34ab9d4bf9be
Jamil Gafur <jamilggafur@gmail.com>
2025-02-20 14:22:06 -0600

    Added cleaning and post processing (#20)
    * adding post processing
    
    * adding post processing
    
    * updating instiue code
    
    * using named modules
    
    * running code
    
    * updates
    
    * moving
    
    * updated neuron similarity and moved post analysis
    
    * small changed
    
    * updating analysis
    
    * post proc and updating runs to do proper pruning
    
    ---------
    
    Co-authored-by: jamil gafur <jamil.gafur@nrel.gov>

--- prune_job.sh
+++ prune_job.sh
@@ -14,19 +14,17 @@ conda activate /scratch/jgafur/LTH_Conda_ENV/LTH_exp_env
 
 # Set model from the first command-line argument
 model=$1
-pruner_percent=$2
 
 # Define available models
 available_models=("LeNet" "ResNet20" "Vgg16")
 
 # Check if a model argument is provided
-if [ -z "$2" ]; then
+if [ -z "$1" ]; then
     echo "Error: No model specified."
-    echo "Usage: $0 <model_name> <pruner_percent>"
+    echo "Usage: $0 <model_name> "
     echo "Available models: ${available_models[@]}"
     exit 1
 fi
-
 
 # Check if the provided model is valid
 if [[ ! " ${available_models[@]} " =~ " ${model} " ]]; then
@@ -46,7 +44,7 @@ sleep 10
 
 # Submit experiment jobs after pruning completes
 echo "Submitting experiment jobs for model: ${model} for job id: ${SLURM_JOB_ID}"
-sbatch experiment_job.sh  ${model}  /scratch/jgafur/LTH_output  NeuronSimilarity  ${pruner_percent} ${SLURM_JOB_ID} 
-sbatch experiment_job.sh  ${model}  /scratch/jgafur/LTH_output  NeuronZeroing  ${pruner_percent} ${SLURM_JOB_ID}
-sbatch experiment_job.sh  ${model}  /scratch/jgafur/LTH_output  WeightZeroing   ${pruner_percent} ${SLURM_JOB_ID}
+sbatch experiment_job.sh  ${model}  /scratch/jgafur/LTH_output  NeuronSimilarity  ${SLURM_JOB_ID} 
+sbatch experiment_job.sh  ${model}  /scratch/jgafur/LTH_output  NeuronZeroing   ${SLURM_JOB_ID}
+sbatch experiment_job.sh  ${model}  /scratch/jgafur/LTH_output  WeightZeroing    ${SLURM_JOB_ID}
 echo "End Time: $(date)"
