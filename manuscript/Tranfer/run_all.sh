338e57c64a5bbdb4ab69f05f931e92a93759ee33
Jamil Gafur <jgafur@argon-itf-login-3.hpc>
2025-10-16 10:04:43 -0500

    nightly run

--- manuscript/Tranfer/run_all.sh
+++ manuscript/Tranfer/run_all.sh
@@ -8,15 +8,16 @@ datasets=("Cifar10" "Cifar100" "TinyImageNet")
 for model in "${models[@]}"; do
   # Define the experiment names inside the model loop
   if [[ "$model" == "VGG16" ]]; then
-    experiment_names=("Original Model" "Last 2" "Stage 4" "Stage 3" "Stage 4-5" "Stage 3-5" "Stage 2-5")
+    experiment_names=("Original Model"  "Last 2" "Stage 4" "Stage 3")
+# "Stage 4-5" "Stage 3-5" "Stage 2-5")
   else
     # If model is RegNetX_400MF, define valid experiments only
     experiment_names=(
       "Original Model" "Last 2"
-      "Stage 4" "Stage 3" "Stage 2" "Stage 1"
-      "Stage 4-5" "Stage 3-5" "Stage 2-5" "Stage 1-5"
-      "Stage 4 last 2 conv" "Stage 3 last 2 conv" "Stage 2 last 2 conv" "Stage 1 last 2 conv"
-      "Stage 4 first 2 conv" "Stage 3 first 2 conv" "Stage 2 first 2 conv" "Stage 1 first 2 conv"
+      #"Stage 4" "Stage 3" "Stage 2" "Stage 1"
+      #"Stage 4-5" "Stage 3-5" "Stage 2-5" "Stage 1-5"
+      #"Stage 4 last 2 conv" "Stage 3 last 2 conv" "Stage 2 last 2 conv" "Stage 1 last 2 conv"
+      #"Stage 4 first 2 conv" "Stage 3 first 2 conv" "Stage 2 first 2 conv" "Stage 1 first 2 conv"
     )
   fi
 
@@ -25,7 +26,7 @@ for model in "${models[@]}"; do
     # Loop through experiment names
     for experiment in "${experiment_names[@]}"; do
       # Loop through flags
-      for flag in "JF" "Kevin"; do
+      for flag in "Kevin"; do
         # Properly quote the variables with spaces and commas
         command="qsub -q all.q -l ngpus=1 -v MODEL=\"$model\",DATASET=\"$dataset\",EXPERIMENT=\"$experiment\",FLAG=\"$flag\" submit_job.pbs"
         
