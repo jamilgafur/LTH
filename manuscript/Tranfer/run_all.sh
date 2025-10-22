9d78ea78803f38900c62b49721521963b67a4592
jamil gafur <jamilggafur@gmail.com>
2025-10-22 11:57:24 -0500

    both models

--- manuscript/Tranfer/run_all.sh
+++ manuscript/Tranfer/run_all.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Define models and datasets
-models=("VGG16" )
-# "RegNetX_400MF")
-datasets=("Cifar10")
-# "Cifar100" )
+models=("VGG16" "RegNetX_400MF")
+datasets=("Cifar10" "Cifar100" )
 
 # Loop through models
 for model in "${models[@]}"; do
