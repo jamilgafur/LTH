b35576dc9597b9dc6e131330c75c642e761c4044
jamil gafur <jamilggafur@gmail.com>
2025-10-31 12:09:07 -0500

    cleaning

--- manuscript/Tranfer/run_all.sh
+++ manuscript/Tranfer/run_all.sh
@@ -4,22 +4,18 @@
 models=("VGG16" "RegNetX_400MF")
 datasets=("Cifar10" "Cifar100")
 
-# Define the experiments dictionary for each model and dataset
-declare -A experiments
-experiments["VGG16_Cifar10"]=( \
-  "Original Model" "Last 2" "Stage 5" "Stage 4" "Stage 3" "Stage 4-5" "Stage 3-5" "Stage 2-5"
-)
-experiments["VGG16_Cifar100"]=( \
-  "Original Model" "Last 2" "Stage 5" "Stage 4" "Stage 3" "Stage 4-5" "Stage 3-5" "Stage 2-5"
-)
-experiments["RegNetX_400MF_Cifar10"]=( \
-  "Original Model" "Last 2" "Stage 4" "Stage 3" "Stage 2" "Stage 4-5" "Stage 3-5" "Stage 2-5"  \
-  "Stage 4 last 2 conv" "Stage 3 last 2 conv" "Stage 2 last 2 conv" "Stage 4 first 2 conv" "Stage 3 first 2 conv" "Stage 2 first 2 conv"
-)
-experiments["RegNetX_400MF_Cifar100"]=( \
-  "Original Model" "Last 2" "Stage 4" "Stage 3" "Stage 2" "Stage 4-5" "Stage 3-5" "Stage 2-5" \
-  "Stage 4 last 2 conv" "Stage 3 last 2 conv" "Stage 2 last 2 conv" "Stage 4 first 2 conv" "Stage 3 first 2 conv" "Stage 2 first 2 conv"
-)
+# Define the experiments for each model and dataset directly as arrays
+
+# VGG16 Experiments
+VGG16_Cifar10=("Original Model" "Last 2" "Stage 5" "Stage 4" "Stage 3" "Stage 4-5" "Stage 3-5" "Stage 2-5")
+VGG16_Cifar100=("Original Model" "Last 2" "Stage 5" "Stage 4" "Stage 3" "Stage 4-5" "Stage 3-5" "Stage 2-5")
+
+# RegNetX_400MF Experiments
+RegNetX_400MF_Cifar10=("Original Model" "Last 2" "Stage 4" "Stage 3" "Stage 2" "Stage 4-5" "Stage 3-5" "Stage 2-5"
+"Stage 4 last 2 conv" "Stage 3 last 2 conv" "Stage 2 last 2 conv" "Stage 4 first 2 conv" "Stage 3 first 2 conv" "Stage 2 first 2 conv")
+
+RegNetX_400MF_Cifar100=("Original Model" "Last 2" "Stage 4" "Stage 3" "Stage 2" "Stage 4-5" "Stage 3-5" "Stage 2-5"
+"Stage 4 last 2 conv" "Stage 3 last 2 conv" "Stage 2 last 2 conv" "Stage 4 first 2 conv" "Stage 3 first 2 conv" "Stage 2 first 2 conv")
 
 # Loop through models
 for model in "${models[@]}"; do
@@ -28,11 +24,22 @@ for model in "${models[@]}"; do
     # Construct the experiment key (model and dataset combination)
     experiment_key="${model}_${dataset}"
 
-    # Get the list of experiments for the current model and dataset
-    experiment_names="${experiments[$experiment_key]}"
+    # Select the experiment array based on the current model and dataset
+    if [[ "$experiment_key" == "VGG16_Cifar10" ]]; then
+      experiment_names=("${VGG16_Cifar10[@]}")
+    elif [[ "$experiment_key" == "VGG16_Cifar100" ]]; then
+      experiment_names=("${VGG16_Cifar100[@]}")
+    elif [[ "$experiment_key" == "RegNetX_400MF_Cifar10" ]]; then
+      experiment_names=("${RegNetX_400MF_Cifar10[@]}")
+    elif [[ "$experiment_key" == "RegNetX_400MF_Cifar100" ]]; then
+      experiment_names=("${RegNetX_400MF_Cifar100[@]}")
+    else
+      echo "Unknown model-dataset combination: $experiment_key"
+      continue
+    fi
 
     # Loop through experiment names
-    for experiment in ${experiment_names[@]}; do
+    for experiment in "${experiment_names[@]}"; do
       # Loop through flags
       for flag in "JF" "Kevin"; do
         # Construct the command with appropriate variables
