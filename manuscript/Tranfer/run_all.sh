09e27855d66383587691c13f0fab98586f58a85e
Jamil Gafur <jgafur@argon-login-6.hpc>
2025-11-09 21:34:01 -0600

    cleaning

--- manuscript/Tranfer/run_all.sh
+++ manuscript/Tranfer/run_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define models and datasets
-models=( "RegNetX_400MF")
-datasets=("Cifar10")
+models=( "RegNetX_400MF" "VGG16")
+datasets=("Cifar100" "Cifar10")
 
 # Define the experiments for each model and dataset directly as arrays
 
@@ -11,7 +11,7 @@ VGG16_Cifar10=("Original Model" "Last 2" "Stage 5" "Stage 4" "Stage 3" "Stage 4-
 VGG16_Cifar100=("Original Model" "Last 2" "Stage 5" "Stage 4" "Stage 3" "Stage 4-5" "Stage 3-5" "Stage 2-5")
 
 # RegNetX_400MF Experiments
-RegNetX_400MF_Cifar10=("Original Model" "Last 2" "Stage 4" "Stage 3" "Stage 2" "Stage 4-5" "Stage 3-5" "Stage 2-5"
+RegNetX_400MF_Cifar10=("Original Model" "Last 2" "Stage 4" "Stage 3" "Stage 2" "Stage 1" "Stage 1-4" "Stage 2-4" "Stage 3-4"
 "Stage 4 last 2 conv" "Stage 3 last 2 conv" "Stage 2 last 2 conv" "Stage 4 first 2 conv" "Stage 3 first 2 conv" "Stage 2 first 2 conv")
 
 RegNetX_400MF_Cifar100=("Original Model" "Last 2" "Stage 4" "Stage 3" "Stage 2" "Stage 4-5" "Stage 3-5" "Stage 2-5"
@@ -44,7 +44,7 @@ for model in "${models[@]}"; do
       # Loop through flags
       for flag in "JF" "Kevin"; do
         # Construct the command with appropriate variables
-        command="qsub -q UI-GPU -l ngpus=1 -v MODEL=\"$model\",DATASET=\"$dataset\",EXPERIMENT=\"$experiment\",FLAG=\"$flag\" submit_job.pbs"
+        command="qsub -q all.q -l ngpus=1 -v MODEL=\"$model\",DATASET=\"$dataset\",EXPERIMENT=\"$experiment\",FLAG=\"$flag\" submit_job.pbs"
         
         # Echo the command for debugging
         echo "Executing: $command"
