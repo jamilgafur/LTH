6ad3450e9e111427815a14c4a3c8ac4542140963
jamil gafur <jamilggafur@gmail.com>
2025-11-22 14:25:06 -0600

    clean

--- manuscript/structured_study/main_experiment.py
+++ manuscript/structured_study/main_experiment.py
@@ -202,6 +202,7 @@ def initialize_pruner(model: nn.Module, train_loader: DataLoader, test_loader: D
         )
 
     print("Running pruner")
+    pretrained_path = "Checkpoint_000000.pth"
     pruner.run(path=save_dir + "/" + pretrained_path)
     print("Pruning process complete.")
     return pruner
