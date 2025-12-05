438173d26e53f10c69583f157c4de1e12422d9b2
jamil gafur <jamilggafur@gmail.com>
2025-12-05 14:49:37 -0600

    cleaning

--- manuscript/Tranfer/run_all.sh
+++ manuscript/Tranfer/run_all.sh
@@ -24,42 +24,44 @@ RegNetX_400MF_Imagenet=("Original Model" "Last 2" "Stage 4" "Stage 3" "Stage 2" 
 
 RegNetX_400MF_Tinyimagenet=("Original Model" "Last 2" "Stage 4" "Stage 3" "Stage 2" "Stage 1" "Stage 1-4" "Stage 2-4" "Stage 3-4"
 "Stage 4 last 2 conv" "Stage 3 last 2 conv" "Stage 2 last 2 conv" "Stage 4 first 2 conv" "Stage 3 first 2 conv" "Stage 2 first 2 conv")
-# quant = list of true and false
+
+# Quant = list of true and false
 quant=("True" "False")
 rm ~/submit*
 
-for quant_flag in "${quant[@]}"; do
-  # Loop through models
-  for model in "${models[@]}"; do
-    # Loop through datasets
-    for dataset in "${datasets[@]}"; do
-      # Construct the experiment key (model and dataset combination)
-      experiment_key="${model}_${dataset}"
+# Loop through models
+for model in "${models[@]}"; do
+  # Loop through datasets
+  for dataset in "${datasets[@]}"; do
+    # Construct the experiment key (model and dataset combination)
+    experiment_key="${model}_${dataset}"
 
-      # Select the experiment array based on the current model and dataset
-      if [[ "$experiment_key" == "VGG16_Cifar10" ]]; then
-        experiment_names=("${VGG16_Cifar10[@]}")
-      elif [[ "$experiment_key" == "VGG16_Cifar100" ]]; then
-        experiment_names=("${VGG16_Cifar100[@]}")
-      elif [[ "$experiment_key" == "VGG16_imagenet" ]]; then
-        experiment_names=("${VGG16_Imagenet[@]}")
-      elif [[ "$experiment_key" == "VGG16_tinyimagenet" ]]; then
-        experiment_names=("${VGG16_Tinyimagenet[@]}") 
-      elif [[ "$experiment_key" == "RegNetX_400MF_Cifar10" ]]; then
-        experiment_names=("${RegNetX_400MF_Cifar10[@]}")
-      elif [[ "$experiment_key" == "RegNetX_400MF_Cifar100" ]]; then
-        experiment_names=("${RegNetX_400MF_Cifar100[@]}")
-      elif [[ "$experiment_key" == "RegNetX_400MF_imagenet" ]]; then
-        experiment_names=("${RegNetX_400MF_Imagenet[@]}")
-      elif [[ "$experiment_key" == "RegNetX_400MF_tinyimagenet" ]]; then
-        experiment_names=("${RegNetX_400MF_Tinyimagenet[@]}")
-      else
-        echo "Unknown model-dataset combination: $experiment_key"
-        continue
-      fi
+    # Select the experiment array based on the current model and dataset
+    if [[ "$experiment_key" == "VGG16_Cifar10" ]]; then
+      experiment_names=("${VGG16_Cifar10[@]}")
+    elif [[ "$experiment_key" == "VGG16_Cifar100" ]]; then
+      experiment_names=("${VGG16_Cifar100[@]}")
+    elif [[ "$experiment_key" == "VGG16_imagenet" ]]; then
+      experiment_names=("${VGG16_Imagenet[@]}")
+    elif [[ "$experiment_key" == "VGG16_tinyimagenet" ]]; then
+      experiment_names=("${VGG16_Tinyimagenet[@]}")
+    elif [[ "$experiment_key" == "RegNetX_400MF_Cifar10" ]]; then
+      experiment_names=("${RegNetX_400MF_Cifar10[@]}")
+    elif [[ "$experiment_key" == "RegNetX_400MF_Cifar100" ]]; then
+      experiment_names=("${RegNetX_400MF_Cifar100[@]}")
+    elif [[ "$experiment_key" == "RegNetX_400MF_imagenet" ]]; then
+      experiment_names=("${RegNetX_400MF_Imagenet[@]}")
+    elif [[ "$experiment_key" == "RegNetX_400MF_tinyimagenet" ]]; then
+      experiment_names=("${RegNetX_400MF_Tinyimagenet[@]}")
+    else
+      echo "Unknown model-dataset combination: $experiment_key"
+      continue
+    fi
 
-      # Loop through experiment names
-      for experiment in "${experiment_names[@]}"; do
+    # Loop through experiment names
+    for experiment in "${experiment_names[@]}"; do
+      # Loop through quant values (True/False)
+      for quant_flag in "${quant[@]}"; do
         # Loop through flags
         for flag in "JF" "Kevin"; do
           # Construct the command with appropriate variables
@@ -69,7 +71,7 @@ for quant_flag in "${quant[@]}"; do
           echo "Executing: $command"
           
           # Run the command
-          eval "$command"
+          # eval "$command"
         done
       done
     done
