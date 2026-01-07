66d2e097fea64c5928c452afe73d4c8133ae8fac
jamil gafur <jamilggafur@gmail.com>
2026-01-07 09:17:21 -0600

    updates only false quant

--- manuscript/Tranfer/run_all.sh
+++ manuscript/Tranfer/run_all.sh
@@ -42,7 +42,7 @@ MobileNet_Imagenet=("Original Model" "Stage 7" "Stage 6-7" "Stage 5-7" "Stage 4-
 MobileNet_Tinyimagenet=("Original Model" "Stage 7" "Stage 6-7" "Stage 5-7" "Stage 4-7" "Stage 3-7" "Stage 2-7" "Stage 1-7" "Stage 6" "Stage 5" "Stage 4" "Stage 3" "Stage 2" "Stage 1" )
 
 # Quant = list of true and false
-quant=("False", "True")
+quant=("False")
 rm ~/submit*
 
 # Loop through models
