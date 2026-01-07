46960df315875203c56b00e1ab46df992a0ae76c
jamil gafur <jamilggafur@gmail.com>
2026-01-07 09:25:56 -0600

    clean

--- manuscript/Tranfer/transfer.py
+++ manuscript/Tranfer/transfer.py
@@ -500,7 +500,7 @@ def run_experiments_for_dataset(
 # Main function
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, default="XceptionNet", choices=["VGG16", "RegNetX_400MF", "InceptionNet", "XceptionNet", "MobileNet", "ConvNetX"], help="Model architecture to use")
+    parser.add_argument("--model", type=str, default="XceptionNet", choices=["VGG16", "RegNetX_400MF", "InceptionNet", "XceptionNet", "MobileNet", "ConvNeXt"], help="Model architecture to use")
     parser.add_argument("--dataset", type=str, default="Cifar10", help="Dataset to use (Cifar10, Cifar100, ImageNet, TinyImageNet)")
     parser.add_argument("--epochs", type=int, default=1, help="Number of epochs to train for")
     parser.add_argument("--pretrain", type=int, default=10, help="Number of pretraining epochs")
