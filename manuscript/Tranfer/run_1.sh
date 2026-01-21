c30bf1a8f4c1010ad1e29de39509a309350322bc
jamil gafur <jamilggafur@gmail.com>
2026-01-20 22:50:08 -0600

    up

---
+++ manuscript/Tranfer/run_1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define models and datasets
+models=("VGG16" "RegNetX_400MF")
+datasets=("Cifar10" "Cifar100" "tinyimagenet")
+epochs=2
+
+# Loop through each combination of model and dataset and qsub the job
+for model in "${models[@]}"; do
+    for dataset in "${datasets[@]}"; do
+        command="qsub -q all.q -l ngpus=1 -v MODEL=${model},DATASET=${dataset},EPOCHS=${epochs} main_1.pbs"
+        echo "Submitting job with command: $command"
+        eval "$command"
+    done
+done
